10b250083d7957ca49ccd577138cd485587d7437
Naveen Kumar <naveen.kumar@fabhotels.com>
2017-06-17 22:29:53 +0530

    Adding string operations code

---
+++ src/basics/StringOperations.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package basics;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author naveen
+ *
+ * @date 17-Jun-2017
+ */
+public class StringOperations {
+
+	public static void main(String[] args) {
+
+		stringCocatenation();
+	}
+
+	private static void stringCocatenation() {
+		String testString = null;
+
+		System.out.println(ObjectUtils.defaultIfNull(testString, StringUtils.EMPTY).concat("\nhello"));
+	}
+}

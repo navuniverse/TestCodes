f3491a105f19b3b909a14c83a684adb6a1b128da
Naveen Kumar <navuniverse@gmail.com>
2017-11-10 18:02:54 +0530

    Added new methods for string

--- src/basics/StringOperations.java
+++ src/basics/StringOperations.java
@@ -15,16 +15,25 @@ public class StringOperations {
 
 	public static void main(String[] args) {
 
-		stringCocatenation();
-		stringCocatenation("naveen");
+		substringFetch("919716554117");
+		
+		stringConcatenation();
+		stringConcatenation("naveen");
 	}
 
-	private static void stringCocatenation() {
+	private static void stringConcatenation() {
 		String testString = null;
 		System.out.println(ObjectUtils.defaultIfNull(testString, StringUtils.EMPTY).concat("hello"));
 	}
 
-	private static void stringCocatenation(String testString) {
+	private static void stringConcatenation(String testString) {
 		System.out.println(ObjectUtils.defaultIfNull(testString, StringUtils.EMPTY).concat("\nhello"));
+	}
+
+	private static void substringFetch(String testString) {
+		if (testString.length() > 10) {
+			testString = testString.substring(testString.length() - 10);
+		}
+		System.out.println("String with 10 digits: " + testString);
 	}
 }
--- src/c1.java
+++ src/c1.java
@@ -8,62 +8,47 @@
  */
 public class c1 implements int1, int2 {
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        int1 int1 = new c1();
-        int1.a(5, 6);
-        int1.m1(1,1,"hi");
-    }
+	public static void main(String[] args) {
+		int1 int1 = new c1();
+		int1.a(5, 6);
+		//int1.m1(1, 1, "hi");
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see int1#m1()
-     */
-    @Override
-    public void m1() {
-        System.out.println("overided method");
+	@Override
+	public void m1() {
+		System.out.println("overided method");
+	}
 
-    }
+	@Override
+	public void m1(int i) {
+		System.out.println("overloaded method");
+	}
 
-    @Override
-    public void m1(int i) {
-        System.out.println("overloaded method");
-    }
+	@Override
+	public void a(int i, Integer j) {
+		System.out.println(i + " int  " + j);
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see int1#a(int, int)
-     */
-    @Override
-    public void a(int i, Integer j) {
-        System.out.println(i + " int  " + j);
+	@Override
+	public void m1(Object... object) {
+		System.out.println("overloaded method + var-args");
 
-    }
-
-    /* (non-Javadoc)
-     * @see int1#m1(int[])
-     */
-    @Override
-    public void m1(Object... object) {
-        System.out.println("overloaded method + var-args");
-        
-    }
+	}
 }
 
 interface int1 {
 
-    void m1();
+	void m1();
 
-    void m1(int i);
-    void m1(Object... object);
-    void a(int i, Integer j);
-    // void a(Integer i, int j);
+	void m1(int i);
+
+	void m1(Object... object);
+
+	void a(int i, Integer j);
+	// void a(Integer i, int j);
 }
 
 interface int2 {
-    void m1();
+
+	void m1();
 }
